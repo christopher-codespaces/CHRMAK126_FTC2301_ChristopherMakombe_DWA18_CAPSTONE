8e497dd55c608f88f28f40485e0d1690e1eb71f7
SwiftlyBox <paymentsswiftlybox@gmail.com>
2023-08-06 16:08:28 +0200

    changes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
-/** @type {import('next').NextConfig} */
+require('dotenv').config({ path: './.env.local' });
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  // other configuration options
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
